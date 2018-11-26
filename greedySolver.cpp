f5ac61d45cad3c2d80cc1dfc79d8e7bc05498547
Francois Germain <gfrancois@ensta.fr>
2018-11-26 18:58:45 +0100

    GFS: suite du greedySolver

--- greedySolver.cpp
+++ greedySolver.cpp
@@ -18,14 +18,14 @@ bool GreedySolver::solve() {
   // liste triée par déséquilibre des stations
   map<int, Station*> stations_triees;
   for(int i = 0; i < inst->stations.size(); i++){
-    stations_triees(pair<int, Station*>(i, inst->stations[i]));
+    stations_triees[i] = inst->stations[i];
   }
 
   // tri de la liste (tri à bulles)
   for(int i = 0; i < stations_triees.size()-1; i++) {
     for(int j = i; j < stations_triees.size()-1; j++) {
       if(stations_triees[j]->deficit() <= stations_triees[j+1]->deficit()) {
-        Station* tmp_station = station_triees[j];
+        Station* tmp_station = stations_triees[j];
         stations_triees[j]   = stations_triees[j+1];
         stations_triees[j+1] = tmp_station;
       }
@@ -43,9 +43,13 @@ bool GreedySolver::solve() {
     int diff = tmp_station->deficit();
 
     // boucle sur les remorques
+    pair<int, int> candidat; // <id du circuit ; manque du camion>
+    candidat.first = -1;
+    candidat.second = -1;
+    bool candidate_found = false;
     for(int j = 0; j < inst->remorques.size(); j++) {
       Circuit* tmp_circuit = sol->circuits[j];
-      int charge_max = tmp_circuit->charge_max;
+      int charge_max = tmp_circuit->remorque->capa;
       Station* latest_station = tmp_circuit->stations[tmp_circuit->stations.size()-1];
       int charge_actuelle = tmp_circuit->charges[latest_station];
 
@@ -53,12 +57,45 @@ bool GreedySolver::solve() {
       if(diff >= 0) {
           // le camion peut fournir les vélos, il est choisi !
         if(charge_max - charge_actuelle < diff) {
-          tmp_circuit->push_back(tmp_station);
-        }
-      } else {
+          tmp_circuit->stations.push_back(tmp_station);
+          tmp_circuit->equilibrate_eleve(); // TODO verifier que ça va bien ici !
+          candidate_found = true;
+          break;
 
+        } // sinon, on le met comme candidat s'il est meilleur que le précédent
+        else {
+          if(diff - charge_max + charge_actuelle < candidat.second) {
+            candidat.first = j;
+            candidat.second = diff - charge_max + charge_actuelle;
+          }
+        }
+      } // déficit de vélos dans la stations
+      else {
+        // le camion peut donner les vélos : il est choisi !
+        if(charge_actuelle > abs(diff)) {
+          tmp_circuit->stations.push_back(tmp_station);
+          tmp_circuit->equilibrate_eleve(); // TODO : vérifier que ça va bien ici
+          candidate_found = true;
+          break;
+        } // sinon, on l'ajoute en candidat
+        else {
+          // notre camion a plus à donner que le candidat actuel
+          if(candidat.second < charge_actuelle) {
+            candidat.first = j;
+            candidat.second = charge_actuelle;
+          }
+        }
       }
 
+    }
+
+    /*
+     *  si on a pas trouvé de candidat fonctionnant dirctement, on ajoute
+     * la station au candidat qui convient "le mieux"
+     */
+
+    if(!candidate_found) {
+      sol->circuits[candidat.first]->stations.push_back(tmp_station);
     }
   }
 
